ad68302b80a14287b6f3c8ccc8ce9af08d58f5d5
Carlos Del Castillo M <maussac13@gmail.com>
2023-02-14 14:11:03 -0500

    backend funcionando

--- src/main/java/com/softcaribbean/VeterinariaBack/VeterinariaBackApplication.java
+++ src/main/java/com/softcaribbean/VeterinariaBack/VeterinariaBackApplication.java
@@ -1,6 +1,5 @@
 package com.softcaribbean.VeterinariaBack;
 
-import com.softcaribbean.VeterinariaBack.config.FiltrosCors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -11,15 +10,9 @@ import org.springframework.core.Ordered;
 public class VeterinariaBackApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VeterinariaBackApplication.class, args);
 	}
-	@Bean
-	public FilterRegistrationBean<FiltrosCors> corsFilterRegistration() {
-		FilterRegistrationBean<FiltrosCors> registrationBean = new FilterRegistrationBean<>(new FiltrosCors());
-		registrationBean.setName("filtros CORS");
-		registrationBean.addUrlPatterns("/*");
-		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return registrationBean;
-	}
+
 
 }
